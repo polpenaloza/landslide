e1241807d90422542a3a2046a312efd1acd718bc
polpenaloza <polpenaloza@gmail.com>
2021-01-24 19:18:36 +0100

    feat(app): theme and main layout

--- apps/sumbank/src/main.tsx
+++ apps/sumbank/src/main.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
 
-import App from './app/app';
+import App from './app/app'
+import theme from './theme'
+
+const extendedTheme = extendTheme(theme)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ColorModeScript initialColorMode={extendedTheme.config.initialColorMode} />
+    <ChakraProvider theme={extendedTheme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
---
+++ apps/sumbank/src/theme.ts
@@ -0,0 +1,24 @@
+// theme.js
+export default {
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: 'false',
+  },
+  colors: {
+    transparent: 'transparent',
+    black: '#000',
+    white: '#fff',
+    brand: {
+      50: '#f0eaff',
+      100: '#d1c1f4',
+      200: '#b199e7',
+      300: '#9171dc',
+      400: '#7248d0',
+      500: '#592fb7',
+      600: '#45248f',
+      700: '#311968',
+      800: '#1e0f40',
+      900: '#0c031b',
+    },
+  },
+}
